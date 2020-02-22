6822607240865d6a155a6913f0a56a57ac8f3685
intrapol <serdarrasimcimen@gmail.com>
2020-02-22 12:13:13 +0300

    anasayfa eklemeler yapıldı

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="dosya/jqu.js"></script>
+    <link rel="stylesheet" href="dosya/boost.css">
+    <title>Restorant sparis verme</title>
+</head>
+<style>
+#rows  {
+    height:25px;
+}
+#masa { 
+    height:80px;
+    margin:2px;
+    font-size:35px;
+    border-radius:15px;
+}
+</style>
+<body>
+<div class="container-fluid " id="rows">
+<div class="row table-dark">
+<div class="col-md-3  border-right " id="rows">doluluk oranı</div> 
+<div class="col-md-3  border-right"id="rows">ss</div> 
+<div class="col-md-3  border-right"id="rows">ss</div> 
+<div class="col-md-3  border-right"id="rows">ss</div> 
+
+</div>
+<div class="row " >
+<div class="col-md-2 border mx-auto  p-2 text-cneter bg-danger" id="masa">ss</div>
+<div class="col-md-2 border mx-auto p-2 text-center"id="masa">ss</div>
+<div class="col-md-2 border mx-auto p-2 text-center" id="masa">ss</div>
+<div class="col-md-2 border mx-auto p-2 text-center" id="masa">ss</div>
+<div class="col-md-2 border mx-auto p-2 text-center"  id="masa">ss</div>
+<div class="col-md-2 border mx-auto p-2 text-center" id="masa">ss</div>
+
+
+</div>
+
+</div>
+</body>
+</html>
